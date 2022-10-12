6df4dc3e46800da9ed510ef7e754f2b1f8a9cd09
David Ongky <da.ongky88@gmail.com>
2022-10-12 19:23:34 +0700

    hilangin katanya pas jumpscare

--- javascript.js
+++ javascript.js
@@ -7,8 +7,10 @@ function gameEnded(){
     clearInterval(downloadTimer);
     var take = document.getElementById("Gameatas");
     var bonehide = document.getElementById("bones");
+    var steal = document.getElementById("steal");
     bonehide.style.display = "none";
     take.style.display = "none";
+    steal.style.display = "none";
     var gambar = document.getElementById("anjing");
     gambar.setAttribute("src","assets/scarydwg.jpeg");
     gambar.style.animation= "shake 0.5s";
